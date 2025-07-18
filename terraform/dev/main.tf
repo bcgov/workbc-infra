7bf54f1f36293f2a8bc4d45abdd0600d04f417b3
Jack Tang <jinchuant@gmail.com>
2025-07-18 14:48:30 -0700

    sg

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -2,12 +2,3 @@
 locals {
   common_tags        = var.common_tags
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-workbc"
- 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
