11837dbab42012f624321592b04e74e2f6f93252
Jack Tang <jinchuant@gmail.com>
2025-07-18 14:57:18 -0700

    security group

--- terraform/dev/security.tf
+++ terraform/dev/security.tf
@@ -15,7 +15,7 @@ data "aws_security_group" "data" {
 resource "aws_security_group" "allow_nfs" {
   name        = "allow_nfs"
   description = "Allow NFS inbound traffic and all outbound traffic"
-  vpc_id      = data.aws_vpc.id
+  vpc_id      = data.aws_vpc.main.id
 
   tags = {
     Name = "allow_nfs"
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_nfs" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_nfs_ipv4" {
   security_group_id = aws_security_group.allow_nfs.id
-  cidr_ipv4         = data.aws_vpc.cidr_block
+  cidr_ipv4         = data.aws_vpc.main.cidr_block
   from_port         = 2049
   ip_protocol       = "tcp"
   to_port           = 2049
@@ -39,7 +39,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
 resource "aws_security_group" "allow_postgres" {
   name        = "allow_postgres"
   description = "Allow Postgres inbound traffic and all outbound traffic"
-  vpc_id      = data.aws_vpc.id
+  vpc_id      = data.aws_vpc.main.id
 
   tags = {
     Name = "allow_postgres"
@@ -48,7 +48,7 @@ resource "aws_security_group" "allow_postgres" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_postgres_ipv4" {
   security_group_id = aws_security_group.allow_postgres.id
-  cidr_ipv4         = data.aws_vpc.cidr_block
+  cidr_ipv4         = data.aws_vpc.main.cidr_block
   from_port         = 5432
   ip_protocol       = "tcp"
   to_port           = 5432
