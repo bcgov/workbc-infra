7aa284c236d061184b6fecd47969507778e8cb29
Jack Tang <jinchuant@gmail.com>
2025-07-21 09:40:31 -0700

    subnets

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,4 +1,9 @@
 
 locals {
   common_tags        = var.common_tags
+  environment		 = var.vpc_name
+  availability_zones = ["A", "B"]
+  app_subnet_names   = [for az in local.availability_zones : "${local.environment}-App-${az}"]
+  data_subnet_names   = [for az in local.availability_zones : "${local.environment}-Data-${az}"]
+  web_subnet_names   = [for az in local.availability_zones : "${local.environment}-Web-${az}"]
 }
--- terraform/dev/network.tf
+++ terraform/dev/network.tf
@@ -6,3 +6,56 @@ data "aws_vpc" "main" {
     values = [var.vpc_name]
   }
 }
+
+data "aws_subnets" "app" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = local.app_subnet_names
+  }
+}
+
+data "aws_subnets" "data" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = local.data_subnet_names
+  }
+}
+
+data "aws_subnets" "web" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = local.web_subnet_names
+  }
+}
+
+data "aws_subnet" "app" {
+  for_each = toset(data.aws_subnets.app.ids)
+  id       = each.value
+}
+
+data "aws_subnet" "data" {
+  for_each = toset(data.aws_subnets.data.ids)
+  id       = each.value
+}
+
+data "aws_subnet" "web" {
+  for_each = toset(data.aws_subnets.web.ids)
+  id       = each.value
+}
+
+
