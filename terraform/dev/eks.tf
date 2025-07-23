e85120520b79e08d83df64beb01363e70af6784b
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2025-07-23 08:14:09 -0700

    Update eks.tf

--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -232,7 +232,8 @@ resource "aws_iam_role_policy" "ses_mailer_policy" {
               "Effect": "Allow",
               "Action": [
                   "ses:SendEmail",
-				  "ses:SendRawEmail"
+		  "ses:SendRawEmail",
+		  "ses:ListIdentities"
               ],
               "Resource": "*"
           }
