c06ded4d06dabb34e92a3c7cb96b9e7b22ae3cca
Jack Tang <jinchuant@gmail.com>
2025-08-19 14:51:13 -0700

    ALB prod

--- terraform/prod/alb.tf
+++ terraform/prod/alb.tf
@@ -123,5 +123,47 @@ resource "aws_security_group_rule" "allow_alb" {
   source_security_group_id = aws_security_group.alb_sg.id
 }
 
+resource "aws_alb_target_group" "cdq" {
+  name                 = "cdq-target-group"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpc.main.id
+  target_type          = "ip"
+  deregistration_delay = 30
+
+  health_check {
+    healthy_threshold   = "5"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "5"
+    path                = "/index.html"
+    unhealthy_threshold = "2"
+  }
+    
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = var.common_tags
+}
+
+resource "aws_lb_listener_rule" "host_based_weighted_routing2" {
+  listener_arn = aws_lb_listener.https_listener.arn
+  priority     = 110
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.cdq.arn
+  }
+
+  condition {
+    host_header {
+      values = ["workbc-cdq.*"]
+    }
+  }
+    
+}
+
 
 
