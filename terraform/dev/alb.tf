97f0c9d2ef545c570e04a135e72084a5efe51f7b
Jack Tang <jinchuant@gmail.com>
2025-11-07 09:19:14 -0800

    TG for workbc

--- terraform/dev/alb.tf
+++ terraform/dev/alb.tf
@@ -151,3 +151,45 @@ resource "aws_lb_listener_rule" "host_based_weighted_routing2" {
   }
     
 }
+
+resource "aws_alb_target_group" "workbc" {
+  name                 = "workbc-target-group"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpc.main.id
+  target_type          = "ip"
+  deregistration_delay = 30
+
+  health_check {
+    healthy_threshold   = "5"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "5"
+    path                = "/index.html"
+    unhealthy_threshold = "2"
+  }
+    
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = var.common_tags
+}
+
+resource "aws_lb_listener_rule" "host_based_weighted_routing3" {
+  listener_arn = aws_lb_listener.https_listener.arn
+  priority     = 120
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.workbc.arn
+  }
+
+  condition {
+    host_header {
+      values = ["workbc.*"]
+    }
+  }
+    
+}
