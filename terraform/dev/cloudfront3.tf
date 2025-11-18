6af698582d8ea984fd6fc8d2628d51a87b7dc23f
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2025-11-18 10:07:24 -0800

    Add files via upload

---
+++ terraform/dev/cloudfront3.tf
@@ -0,0 +1,78 @@
+# cloudfront3.tf - WorkBC
+
+resource "aws_cloudfront_distribution" "workbc-main" {
+
+  count = var.cloudfront ? 1 : 0
+
+  origin {
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = [
+      "TLSv1.2"]
+    }
+
+    domain_name = "workbc.a55eb5-dev.stratus.cloud.gov.bc.ca"
+    origin_id   = random_integer.cf_origin_id.result
+	
+	custom_header {
+	  name = "X-Forwarded-Host"
+	  value = "dev.workbc.ca"
+	}
+	
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "WorkBC.ca"
+
+  default_cache_behavior {
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+    "PUT"]
+    cached_methods = ["GET", "HEAD"]
+
+    target_origin_id = random_integer.cf_origin_id.result
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+	
+    # SimpleCORS
+    response_headers_policy_id = "60669652-455b-4ae9-85a4-c4c02393f86c"
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations = ["CA"]
+    }
+  }
+
+  tags = var.common_tags
+  
+  aliases = ["dev.workbc.ca"]
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:396067939651:certificate/8422cb87-5c47-4dcf-86b3-04a93695fbca"
+    ssl_support_method = "sni-only"
+  }
+}
+
