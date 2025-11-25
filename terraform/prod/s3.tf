310a70d856097ed5c66d7058e9409a0afdf1bc03
Jack Tang <jinchuant@gmail.com>
2025-11-25 10:33:35 -0800

    S3 bucket

---
+++ terraform/prod/s3.tf
@@ -0,0 +1,40 @@
+resource "aws_s3_bucket" "workbc_s3" {
+  bucket = "workbc-bucket"
+}
+
+resource "aws_s3_bucket_acl" "workbc_s3_acl" {
+  bucket = aws_s3_bucket.workbc_s3.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = aws_s3_bucket.workbc_s3.id
+  policy = data.aws_iam_policy_document.allow_access_from_cloudfront.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_cloudfront" {
+
+  statement {
+  
+    sid = "AllowCloudFrontServicePrincipal"
+    
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+	  
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.workbc_s3.arn}/*",
+    ]
+	
+	condition {
+	  test = "StringEquals"
+	  variable = "AWS:SourceArn"
+	  #values = ["${aws_cloudfront_distribution.workbc[0].arn}"]
+	  values = ["arn:aws:cloudfront::396067939651:distribution/E1GDYI52ISK5MS", "arn:aws:cloudfront::318574063652:distribution/E2Y9BDYHIX4JX3"]
+	}
+  }
+
+}
