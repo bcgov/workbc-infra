d2bc7f0bc62244f6e08adaf2dc03518d720f86d6
Jack Tang <jinchuant@gmail.com>
2025-11-25 10:38:56 -0800

    Remove S3 ACL

--- terraform/prod/s3.tf
+++ terraform/prod/s3.tf
@@ -2,10 +2,10 @@ resource "aws_s3_bucket" "workbc_s3" {
   bucket = "workbc-bucket"
 }
 
-resource "aws_s3_bucket_acl" "workbc_s3_acl" {
-  bucket = aws_s3_bucket.workbc_s3.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket_acl" "workbc_s3_acl" {
+#  bucket = aws_s3_bucket.workbc_s3.id
+#  acl    = "private"
+#}
 
 resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
   bucket = aws_s3_bucket.workbc_s3.id
