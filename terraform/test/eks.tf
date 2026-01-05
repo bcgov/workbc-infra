ad7c73468f30ee611d7dfc74eaa2e0d38cfda691
Jack Tang <jinchuant@gmail.com>
2026-01-05 15:24:26 -0800

    SG for ALB

--- terraform/test/eks.tf
+++ terraform/test/eks.tf
@@ -246,3 +246,12 @@ resource "aws_iam_role_policy" "ses_mailer_policy" {
   }
   EOF
 }
+
+resource "aws_security_group_rule" "allow_alb2" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8081
+  protocol                 = "tcp"
+  security_group_id        = data.aws_security_group.eks_node_sg.id
+  source_security_group_id = aws_security_group.alb_sg.id
+}
