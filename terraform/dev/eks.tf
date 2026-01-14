15034c89800a493918f518705ed5251db84779d1
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2026-01-14 15:32:56 -0800

    Update eks.tf

--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -249,8 +249,18 @@ resource "aws_iam_role_policy" "ses_mailer_policy" {
           },
 		  {
 			  "Effect": "Allow",
-			  "Action": "secretsmanager:GetSecretValue",
+			  "Action": [
+				  "secretsmanager:GetSecretValue",
+				  "secretsmanager:DescribeSecret"
+			  ]
 			  "Resource": "arn:aws:secretsmanager:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:secret:*"
+		  },
+		  {
+			  "Effect": "Allow",
+			  "Action": [
+				  "kms:Decrypt"
+			  ]
+			  "Resource": "arn:aws:kms:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:key/*"
 		  }
       ]
   }
