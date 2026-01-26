a7610b67edc0ca79fe82dbdf9189f7bc3b4329da
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2026-01-26 14:30:00 -0800

    Update eks2.tf

--- terraform/dev/eks2.tf
+++ terraform/dev/eks2.tf
@@ -53,32 +53,32 @@ resource "aws_eks_addon" "secrets-manager-addon2" {
 }
 
 #Node group
-#resource "aws_eks_node_group" "eks-ng2" {
-#  cluster_name    = aws_eks_cluster.workbc-cluster2.name
-#  node_group_name = "eks-ng2"
-#  node_role_arn   = aws_iam_role.eks-ng-role.arn
-#  subnet_ids      = data.aws_subnets.app.ids
+resource "aws_eks_node_group" "eks-ng2" {
+  cluster_name    = aws_eks_cluster.workbc-cluster2.name
+  node_group_name = "eks-ng2"
+  node_role_arn   = aws_iam_role.eks-ng-role.arn
+  subnet_ids      = data.aws_subnets.app.ids
 
-#  scaling_config {
-#    desired_size = 3
-#    max_size     = 10
-#    min_size     = 1
-#  }
+  scaling_config {
+    desired_size = 3
+    max_size     = 10
+    min_size     = 1
+  }
 
-#  instance_types = ["t3.large"]
+  instance_types = ["t3.large"]
 
-#  update_config {
-#    max_unavailable = 1
-#  }
+  update_config {
+    max_unavailable = 1
+  }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
- # depends_on = [
- #   aws_iam_role_policy_attachment.ng-AmazonEKSWorkerNodePolicy,
- #   aws_iam_role_policy_attachment.ng-AmazonEKS_CNI_Policy,
- #   aws_iam_role_policy_attachment.ng-AmazonEC2ContainerRegistryReadOnly,
- # ]
-#}
+  depends_on = [
+    aws_iam_role_policy_attachment.ng-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.ng-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.ng-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
 
 
 data "aws_security_group" "eks_node_sg2" {
