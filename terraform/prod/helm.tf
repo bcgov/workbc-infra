24c83401bb99f89cfcdc91e1ac484efbe3023e7c
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2026-05-06 09:14:14 -0700

    Add files via upload (#22)
    * Add files via upload
    
    * Update eks2.tf
    
    * Update eks.tf

---
+++ terraform/prod/helm.tf
@@ -0,0 +1,20 @@
+resource "helm_release" "secrets_store_csi_driver" {
+  name       = "csi-secrets-store"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver"
+  version    = "1.4.3"  # Use latest
+
+  set = [{
+    name  = "syncSecret.enabled"
+    value = "true"
+  }]
+}
+
+resource "helm_release" "secrets_store_csi_aws_provider" {
+  name       = "secrets-store-csi-driver-provider-aws"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+  chart      = "secrets-store-csi-driver-provider-aws"
+  depends_on = [helm_release.secrets_store_csi_driver]
+}
