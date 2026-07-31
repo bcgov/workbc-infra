ce4ce3ba091b0450c9c2ba084c84e8676ec0d43f
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2026-07-31 12:01:41 -0700

    Add files via upload (#28)
    * Add files via upload
    
    * Update cloudfront.tf
    
    * Update cloudfront2.tf
    
    * Update cloudfront3.tf
    
    * Update cloudfront4.tf
    
    * Update cloudfront5.tf
    
    * Update cloudfront6.tf

--- terraform/dev/cloudfront5.tf
+++ terraform/dev/cloudfront5.tf
@@ -71,7 +71,7 @@ resource "aws_cloudfront_distribution" "workbc-jb-adm" {
   aliases = ["dev-admin-jobboard.workbc.ca"]
 
   viewer_certificate {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:396067939651:certificate/8422cb87-5c47-4dcf-86b3-04a93695fbca"
+    acm_certificate_arn = "arn:aws:acm:us-east-1:396067939651:certificate/c0a913cc-fd86-44e0-9096-fbcb7a98044b"
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method = "sni-only"
   }
